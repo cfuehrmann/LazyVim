19d56a6211b1a162cd28dc69c550595cf2ca7dca
cfuehrmann <cfuhrmann@gmail.com>
2023-05-01 19:51:23 +0200

    Enable debugging

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,29 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local function debug_nearest()
+  if vim.bo.filetype == "cs" then
+    require("neotest").run.run({
+      strategy = require("neotest-dotnet.strategies.netcoredbg"),
+      is_custom_dotnet_debug = true,
+    })
+  else
+    require("neotest").run.run({ strategy = "dap" })
+  end
+end
+
+local function debug_file()
+  if vim.bo.filetype == "cs" then
+    require("neotest").run.run({
+      vim.fn.expand("%"),
+      strategy = require("neotest-dotnet.strategies.netcoredbg"),
+      is_custom_dotnet_debug = true,
+    })
+  else
+    require("neotest").run.run({ strategy = "dap" })
+  end
+end
+
 require("which-key").register({
   t = {
     name = "tests",
@@ -23,6 +46,8 @@ require("which-key").register({
       end,
       "Toggle summary",
     },
+    d = { debug_nearest, "Debug nearest" },
+    D = { debug_file, "Debug file" },
   },
   prefix = "<leader>",
 })
---
+++ lua/plugins/dap.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+
+      -- Neotest-dotnet seems hardwired to use an adapter named "netcoredbg".
+      -- The dap.configuration has a choosable "type", which we choose to
+      -- point to this adapter too.
+
+      dap.adapters.netcoredbg = {
+        type = "executable",
+        command = vim.fn.expand("$HOME/.local/share/LazyVim/mason/bin/netcoredbg"),
+        args = { "--interpreter=vscode" },
+      }
+
+      dap.configurations.cs = {
+        {
+          type = "netcoredbg",
+          name = "launch - netcoredbg",
+          request = "launch",
+          program = function()
+            return vim.fn.input({
+              prompt = "Path to dll: ",
+              default = vim.fn.getcwd() .. "/",
+              completion = "file",
+            })
+          end,
+        },
+      }
+    end,
+  },
+}
