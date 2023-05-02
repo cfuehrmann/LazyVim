ecf8c521f4c94bf5db268b87eec166d2f9092380
cfuehrmann <cfuhrmann@gmail.com>
2023-05-02 21:38:31 +0200

    Load neotest only when keys are used

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,3 +1,26 @@
+local function debug_nearest()
+  if vim.bo.filetype == "cs" then
+    require("neotest").run.run({
+      strategy = require("neotest-dotnet.strategies.netcoredbg"),
+      is_custom_dotnet_debug = true,
+    })
+  else
+    require("neotest").run.run({ strategy = "dap" })
+  end
+end
+
+local function debug_file()
+  if vim.bo.filetype == "cs" then
+    require("neotest").run.run({
+      vim.fn.expand("%"),
+      strategy = require("neotest-dotnet.strategies.netcoredbg"),
+      is_custom_dotnet_debug = true,
+    })
+  else
+    require("neotest").run.run({ strategy = "dap" })
+  end
+end
+
 return {
   {
     "nvim-neotest/neotest",
@@ -9,6 +32,34 @@ return {
     },
     opts = function(_, opts)
       opts.adapters = { require("neotest-dotnet")({}) }
+    end,
+    keys = function()
+      return {
+        {
+          "<leader>tr",
+          function()
+            require("neotest").run.run()
+          end,
+          desc = "Run nearest",
+        },
+        {
+          "<leader>tR",
+          function()
+            require("neotest").run.run(vim.fn.expand("%"))
+          end,
+          desc = "Run file",
+        },
+        {
+          "<leader>td",
+          debug_nearest,
+          desc = "Debug nearest",
+        },
+        {
+          "<leader>tD",
+          debug_file,
+          desc = "Debug file",
+        },
+      }
     end,
   },
 }
