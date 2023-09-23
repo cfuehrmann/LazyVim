6c134e3fa50a28e2d3152fa91175df4fe7d8325d
cfuehrmann <cfuhrmann@gmail.com>
2023-09-23 22:02:43 +0200

    Enable inlay hints and configure for Lua and TypeScript

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = { enabled = true },
+      servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              hint = { enable = true },
+            },
+          },
+        },
+        tsserver = {
+          settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayEnumMemberValueHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
