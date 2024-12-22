d0f2f24fe7c81abbf0e9448900ca2d01d20a22fe
Carsten Führmann <cfuhrmann@gmail.com>
2024-12-22 15:42:28 +0100

    Clean neotest config

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -2,19 +2,13 @@ return {
   {
     "nvim-neotest/neotest",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "antoinemadec/FixCursorHold.nvim",
       "Issafalcon/neotest-dotnet",
       "rouge8/neotest-rust",
     },
     opts = function(_, opts)
       opts.adapters = {
         require("neotest-dotnet")({}),
-        require("neotest-rust")({
-          args = { "--no-capture" },
-          dap_adapter = "codelldb",
-        }),
+        require("neotest-rust")({}),
       }
     end,
   },
