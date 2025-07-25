9c6dd84304f46ed495c849199f7796e32df05a3e
Carsten Führmann <cfuhrmann@gmail.com>
2025-07-25 12:13:42 +0200

    Fix Avante configuration

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -8,23 +8,29 @@ return {
       -- add any opts here
       -- for example
       provider = "claude",
-      openai = {
-        endpoint = "https://api.openai.com/v1",
-        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-        timeout = 30000, -- timeout in milliseconds
-        temperature = 0, -- adjust if needed
-        max_tokens = 4096,
-        -- reasoning_effort = "high", -- only supported for "o" models
-      },
-      claude = {
-        endpoint = "https://api.anthropic.com",
-        model = "claude-3-7-sonnet-20250219",
-        timeout = 30000, -- Timeout in milliseconds
-        temperature = 0,
-        max_tokens = 8000,
-      },
-      behaviour = {
-        enable_claude_text_editor_tool_mode = true,
+      providers = {
+        openai = {
+          endpoint = "https://api.openai.com/v1",
+          model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+          timeout = 30000, -- timeout in milliseconds
+          extra_request_body = {
+            temperature = 0,
+            max_tokens = 4096,
+          },
+          -- reasoning_effort = "high", -- only supported for "o" models
+        },
+        claude = {
+          endpoint = "https://api.anthropic.com",
+          model = "claude-3-7-sonnet-20250219",
+          timeout = 30000, -- Timeout in milliseconds
+          extra_request_body = {
+            max_tokens = 8000,
+            temperature = 0,
+          },
+        },
+        behaviour = {
+          enable_claude_text_editor_tool_mode = true,
+        },
       },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
